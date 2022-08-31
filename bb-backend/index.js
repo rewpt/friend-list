6ae92c10569f1b667e45bc78ef5c1bd5c2408d3d
rewpt <zpermack@gmail.com>
2022-08-30 21:27:17 -0400

    add middleware for not-found

--- bb-backend/index.js
+++ bb-backend/index.js
@@ -1,2 +1,14 @@
 import express from 'express'
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome!')
+})
+
+
+
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`server listening on port ${port}`))
